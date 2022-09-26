bc2e44433f3cc80b0d9f18c67dcb2258bdb34b5d
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-26 20:50:13 +0200

    Add files via upload

---
+++ ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adiaz-be <adiaz-be@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 19:46:10 by adiaz-be          #+#    #+#             */
+/*   Updated: 2022/09/26 19:46:29 by adiaz-be         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	if (start >= ft_strlen(s))
+	{
+		str = ft_calloc(1, sizeof(char));
+		if (!str)
+			return (0);
+		return (str);
+	}
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	str = ft_calloc(sizeof(char), len + 1);
+	if (!str)
+		return (NULL);
+	while (s[start + i] && i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	return (str);
+}
