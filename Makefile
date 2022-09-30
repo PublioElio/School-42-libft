34b4c4b40e28c74d3896c1d16a8861842a4b554d
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-30 20:44:58 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -24,6 +24,8 @@ SRC	=	ft_atoi.c		\
 		ft_memcpy.c		\
 		ft_memmove.c	\
 		ft_memset.c		\
+		ft_putchar_fd.c	\
+		ft_putstr_fd.c	\
 		ft_split.c		\
 		ft_strchr.c		\
 		ft_strdup.c		\
--- libft.h
+++ libft.h
@@ -34,6 +34,8 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
