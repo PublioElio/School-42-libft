1e3757ed1d5f31b0c3a05414e877fe5eb8094838
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-02 19:17:09 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -47,15 +47,15 @@ SRC	=	ft_atoi.c		\
 
 OBJS = $(SRC:.c=.o)
 
-BONUS =	ft_lstadd_back.c	\
-		ft_lstadd_front.c	\
-		ft_lstclear.c	\
-		ft_lstdelone.c	\
-		ft_lstiter.c	\
-		ft_lstlast.c	\
-		ft_lstmap.c		\
-		ft_lstnew.c		\
-		ft_lstsize.c	\
+BONUS =	ft_lstadd_back_bonus.c	\
+		ft_lstadd_front_bonus.c	\
+		ft_lstclear_bonus.c		\
+		ft_lstdelone_bonus.c	\
+		ft_lstiter_bonus.c		\
+		ft_lstlast_bonus.c		\
+		ft_lstmap_bonus.c		\
+		ft_lstnew_bonus.c		\
+		ft_lstsize_bonus.c		\
 
 BONUS_OBJS = $(BONUS:.c=.o)
 
---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adiaz-be <adiaz-be@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/01 13:52:05 by adiaz-be          #+#    #+#             */
+/*   Updated: 2022/10/01 13:53:00 by adiaz-be         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
